3cfde2bf227994f4125cf82bbf7cea80162c13ee
BusterBrown <123121140+sleepyG8@users.noreply.github.com>
2025-05-17 11:50:23 -0700

    Update StealthDebugger.c
    newlines XD

--- StealthDebugger.c
+++ StealthDebugger.c
@@ -709,8 +709,8 @@ BOOL WINAPI debug(LPCVOID param) {
                                     printf("!params  - Show process parameters (debug status & path)\n");
                                     printf("!proc    - Display all running processes on the system\n");
                                     printf("!bit     - Display Bitfield data\n");
-                                    printf("!mbi     - get mbi info (only works for unprotected process)");
-                                    printf("!synbreak - break at a debug symbol (not stable yet)");
+                                    printf("!mbi     - get mbi info (only works for unprotected process)\n");
+                                    printf("!synbreak - break at a debug symbol (not stable yet)\n");
                                     printf("!break   - Set a break and read registers\n");
                                     printf("clear    - Clear the console screen\n");
                                     printf("exit     - Terminate debugging session\n");
